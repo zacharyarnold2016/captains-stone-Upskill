dc9450fbd9aa0db200eaa13d610f19956deccd4c
Zachary Arnold <Zacharyarnold2016@gmail.com>
2022-07-24 23:55:06 -0400

    creates an Authorization Service for JWTs

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,1 +1,20 @@
-export default class AuthService {}
+import jwt from "jsonwebtoken";
+
+export default class AuthService {
+  static async decodeId(token) {
+    const { user } = jwt.decode(token, "TOP_SECRET");
+    const { id } = user;
+    return id;
+  }
+
+  static async decodeBody(token) {
+    const { user } = jwt.decode(token, "TOP_SECRET");
+    return user;
+  }
+
+  static async decodeRole(token) {
+    const { user } = jwt.decode(token, "TOP_SECRET");
+    const { role } = user;
+    return role;
+  }
+}
